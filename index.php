436e92fa9ac63f95868bc5b2814656d66356ca99
Jelle de Jong <jelle@hoest.nl>
2013-01-18 15:30:43 +0100

    Nieuwe link

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
   header("Content-type: text/html; charset=UTF-8");
-  
+
   include('header.inc');
   if($_GET['p']!=NULL && $_GET['p']!='' && file_exists($_GET['p'].'.inc')) {
     include($_GET['p'].'.inc');
